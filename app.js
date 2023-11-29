12dc89e2c4c5f4a0b10840a6f93f7de41645670d
Chad Hanson <chad.hanson@stellantis.com>
2023-11-29 07:07:55 -0500

    i dont know why these are modified

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-
-const fs = require('fs');
-
-// Create Dino Objects
-const getDinoJson = () => {
-  const dinoData = fs.readFileSync('./dino.json');
-  const jsonData = JSON.parse(dinoData);
-  const dinosArray = jsonData.Dinos.map(dino => {
-    let { species, weight, height, diet, where, when, fact } = dino;
-    return new Dino(species, weight, height, diet, where, when, fact);
-  });
-  return dinosArray;
-};
-
 // Create Dino Constructor
 function Dino(species, weight, height, diet, where, when, fact) {
     this.species = species;
@@ -22,6 +8,19 @@ function Dino(species, weight, height, diet, where, when, fact) {
     this.when = when;
     this.fact = fact;
 }
+// Create Dino Objects
+const path = require('path');
+const fs = require('fs');
+
+const getDinoJson = () => {
+  const dinoData = fs.readFileSync(path.join(__dirname, 'dino.json'));
+  const jsonData = JSON.parse(dinoData);
+  const dinosArray = jsonData.Dinos.map(dino => {
+    let { species, weight, height, diet, where, when, fact } = dino;
+    return new Dino(species, weight, height, diet, where, when, fact);
+  });
+  return dinosArray;
+};
 
 // Create Human Object
 function Human(name, weight, height, diet) {
